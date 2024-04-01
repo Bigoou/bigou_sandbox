d3d4ce0c7837905b1cc4e1b9a751f8e1019c393e
Antoine Droyer <antoine.droyer@gmail.com>
2024-04-01 15:38:31 +0200

    favicon

--- app/head.jsx
+++ app/head.jsx
@@ -34,6 +34,7 @@ export default function Head() {
       <meta property='og:site_name' content={title} />
       <meta property='og:description' content={description} />
 
+      <link rel="icon" href="/icons/favicon.ico" />
       <link rel='apple-touch-icon' href='/icons/apple-touch-icon.png' />
       <link rel='apple-touch-icon' sizes='16x16' href='/icons/favicon-16x16.png' />
       <link rel='apple-touch-icon' sizes='32x32' href='/icons/favicon-32x32.png' />
